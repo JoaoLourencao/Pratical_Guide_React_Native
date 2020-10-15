80f5690d5d4f7ec96f88bdc64bab1d4342a545d0
Joao Lourencao <joao.lourencao@bitzar.solutions>
2020-10-15 18:14:37 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Merry');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <StatusBar style="light"/>
+
+    // <Text style={styles.text}>{outputText}</Text>
+    //   <Button title="clique aqui" onPress={() => setOutputText('pfff')}/>
+    // </View>
+
+
+    <View>
+      <View>
+
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }
@@ -14,8 +29,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'yellow',
   },
 });
